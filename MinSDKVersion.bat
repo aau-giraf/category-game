c8c5b0bfde6e01ed9e670b5d1ec53e7aa96fdb52
Jonas <llamabutlermecha@gmail.com>
2014-03-04 10:11:18 +0100

    Updated to apply changes to project.properties as well

--- MinSDKVersion.bat
+++ MinSDKVersion.bat
@@ -3,3 +3,8 @@ for /r %%F in (*.xml) do (
   type "%%F"|repl "android:minSdkVersion=\q17\q" "android:minSdkVersion=\q15\q" LX >"%%F.new"
   move /y "%%F.new" "%%F" >nul
 )
+
+for /r %%F in (project.properties) do (
+  type "%%F"|repl "target=android-17" "target=android-15" LX >"%%F.new"
+  move /y "%%F.new" "%%F" >nul
+)
