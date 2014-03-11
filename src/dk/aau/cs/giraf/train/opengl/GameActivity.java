c30330fd05b4fb79f4d278ca51c38be285e3a402
Tobias <tobiasfrandsen4@gmail.com>
2014-03-11 12:42:13 +0100

    no message

--- src/dk/aau/cs/giraf/train/opengl/GameActivity.java
+++ src/dk/aau/cs/giraf/train/opengl/GameActivity.java
@@ -250,8 +250,6 @@ public class GameActivity extends Activity {
 
 		// TrainDriver
 		trainDriverLinear = (LinearLayout) findViewById(R.id.TrainDriverLinearLayout);
-
-
 	}
 
 	@Override
@@ -357,8 +355,6 @@ public class GameActivity extends Activity {
     }
 
     public void helpbtnonclick(View view) {
-        findViewById(id.instructions).setVisibility(View.VISIBLE);
-        findViewById(id.instructions).bringToFront();
         if (findViewById(id.instructions).getVisibility() != View.VISIBLE){
             findViewById(id.instructions).setVisibility(View.VISIBLE);
             findViewById(id.instructions).bringToFront();
